e98fef8ef746c8c6682a428f639e4813b74370b9
Max Sharpnack <Maxsharpnack@gmail.com>
2025-10-23 14:00:43 -0500

    spots

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+const editProfileButton = document.querySelector('.profile__edit-button');
+const editProfileModal = document.querySelector('#edit-profile-modal');
+const editProfileCloseButton = editProfileModal.querySelector('.modal__close-button');
+
+const newPostButton = document.querySelector('.profile__add-button');
+const newPostModal = document.querySelector('#new-post-modal');
+const newPostCloseButton = newPostModal.querySelector('.modal__close-button');
+
+function openModal(modal) {
+  modal.classList.add('modal_is-opened');
+}
+
+function closeModal(modal) {
+  modal.classList.remove('modal_is-opened');
+}
+
+editProfileButton.addEventListener('click', () => openModal(editProfileModal));
+editProfileCloseButton.addEventListener('click', () => closeModal(editProfileModal));
+
+newPostButton.addEventListener('click', () => openModal(newPostModal));
+newPostCloseButton.addEventListener('click', () => closeModal(newPostModal));
